0d06a310b54254c641367897fde2cffa0097ad6b
Naiabunner <Okechukwudaniel12@gmail.com>
2023-06-28 22:08:08 +0100

    Added header&Input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { Header } from "./Component/header";
 import './index.css'
 export const App= ()=>{
-  return(<Header/>)
+  return(
+  <><Header/>
+  
+</>)
 }
--- src/Component/header.js
+++ src/Component/header.js
@@ -1,9 +1,28 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear, faGrip } from '@fortawesome/free-solid-svg-icons'
 
 export const Header =()=>{
     return(
         <div className="head">
-            HEAD
+            <ul className='ul_head'>
+                <li>
+                    <img src="./images/glogo.png
+                    " alt="" width="100px"  className= 'google_logo'/>
+                </li>
+                <li>
+                    <div className="input_bar">
+                        <input type="text" className='search_input'/>
+                        <img src="./images/voice.png" alt="10px" width="10px"/>
+                        <img src="./images/picture.png" alt="40px" width="30px"/>
+                        <i className="fa-solid fa-magnifying-glass" id='search_btn'></i>
+                    </div>
+                </li>
+            </ul>
+            <div className="setting_grid-profile">
+                        <FontAwesomeIcon icon={faGear} className='setting'/>
+                        <FontAwesomeIcon icon={faGrip} className='setting'/>
+                        <img src="./images/dog.jpg" alt="40px" width="40px" className='profile'/>
+            </div>   
         </div>
     )
 }
